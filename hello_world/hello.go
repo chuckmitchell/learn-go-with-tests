c9c56bc31c89647a2e4cea62f8ab58f2c22d8450
Charles Mitchell <charles.mitchell@greenshield.ca>
2026-09-08 19:32:54 -0300

    Refactor Hello function to use a constant for greeting prefix

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
 	if name == "" {
 		name = "world"
 	}
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 func main() {
