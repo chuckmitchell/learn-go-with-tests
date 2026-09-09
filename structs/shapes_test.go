6c5fa225784ed3522eb1df73fed0fb778058196d
Charles Mitchell <charles.mitchell@greenshield.ca>
2026-09-09 17:55:37 -0300

    Completed structs module

---
+++ structs/shapes_test.go
@@ -0,0 +1,36 @@
+package structs
+
+import "testing"
+
+func TestPerimiter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.f, want %2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		name         string
+		shape        Shape
+		expectedArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, expectedArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, expectedArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Width: 12, Height: 6}, expectedArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			calculatedArea := tt.shape.Area()
+			if calculatedArea != tt.expectedArea {
+				t.Errorf("%#v area calculated: %g, expected: %g", tt.shape, calculatedArea, tt.expectedArea)
+			}
+		})
+	}
+
+}
