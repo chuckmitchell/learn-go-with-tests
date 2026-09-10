0873068a72a675ee081b2ee73ee219bb4ba8ff13
Charles Mitchell <charles.mitchell@greenshield.ca>
2026-09-09 22:04:30 -0300

    Add dictionary functionality with search method and corresponding tests

---
+++ maps/dictionary_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is a test"}
+	got := dictionary.Search("test")
+	want := "this is a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q, given %q", got, want, "test")
+	}
+}
