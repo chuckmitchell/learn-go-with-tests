5193afa98894906bceb9673cc960aaef45a68455
Charles Mitchell <charles.mitchell@greenshield.ca>
2026-09-09 22:18:23 -0300

    Refactor dictionary search method to return an error for unknown words and update tests to cover both known and unknown cases

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,18 +1,43 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is a test"}
-	got := dictionary.Search("test")
-	want := "this is a test"
 
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
+
 }
 
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q, given %q", got, want, "test")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if !errors.Is(got, want) {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
